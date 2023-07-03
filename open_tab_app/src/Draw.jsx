e420d659dd85412c22a2f544e94969d54401e630
Julius Steen <julius.max@me.com>
2023-07-03 22:42:47 +0200

    Improve clash display

--- open_tab_app/src/Draw.jsx
+++ open_tab_app/src/Draw.jsx
@@ -116,15 +116,15 @@ function bucketIssuesBySeverity(issues) {
 function ClashIndicator(props) {
   let issueBuckets = bucketIssuesBySeverity(props.issues);
 
-  return <div className="flex h-6 rounded-md overflow-hidden w-12 border border-gray-600" onMouseEnter={() => props.onHover(true)} onMouseLeave={() => props.onHover(false)}>
+  return <div className="font-mono font-bold flex h-6 rounded-md overflow-hidden w-16 border border-gray-600 text-xs" onMouseEnter={() => props.onHover(true)} onMouseLeave={() => props.onHover(false)}>
     {
       props.issues.length == 0 ?
-      <div className="h-full flex-1 items-center bg-green-500 text-white text-sm pl-1 pr-1">{"\u2713"}</div>
+      <div className="h-full flex-1 flex items-center justify-center bg-green-500 text-white pl-1 pr-1 text-lg">{"\u2713"}</div>
       :
       ["misc", "low", "mid", "high"].map(
         (key) => {
           return issueBuckets[key].length > 0 ?
-          <div key={key} className={`h-full flex flex-1 items-center text-white text-sm pl-1 pr-1 ${ISSUE_COLORS_BG[key]}`}>{issueBuckets[key].length}</div>
+          <div key={key} className={`h-full flex flex-1 items-center justify-center text-white pl-1 pr-1 ${ISSUE_COLORS_BG[key]}`}>{issueBuckets[key].length <= 9 ? issueBuckets[key].length : <span className="text-[8px]">&gt;9</span>}</div>
           :
           null
         }
