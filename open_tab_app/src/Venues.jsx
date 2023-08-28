8fa183d02c120283005e61d464758afae178f8a2
Julius Steen <julius.max@me.com>
2023-08-28 22:20:20 +0200

    Add ability to add venues manually

---
+++ open_tab_app/src/Venues.jsx
@@ -0,0 +1,59 @@
+import { useContext } from "react";
+import { useView } from "./View"
+import { TournamentContext } from "./TournamentContext";
+import { SortableTable, EditableCell } from "./SortableTable";
+import { executeAction } from "./Action";
+import Button from "./Button";
+
+
+
+export default function VenueOverview() {
+    let tournamentId = useContext(TournamentContext).uuid;
+    let venues = useView({type: "Venues", tournament_uuid: tournamentId}, {"venues": []});
+    return <div className="h-full w-full flex flex-col">
+        <div className="w-full flex-1">
+            <SortableTable
+                rowId={"uuid"}
+                data={
+                    venues.venues
+                }
+                columns={
+                    [
+                        {
+                            "key": "ordering_index",
+                            "header": "#",
+                            cellFactory: (value, rowIdx, colIdx, rowValue) => {
+                                return <EditableCell key={colIdx} value={value} onChange={
+                                    (newIndex) => {
+                                        let newVenue = {... rowValue};
+                                        newVenue.ordering_index = parseInt(newIndex);
+                                        executeAction("UpdateVenues", {updated_venues: [newVenue], tournament_id: tournamentId})
+                                    }
+                                } />
+                            }      
+                        },
+                        {
+                            "key": "name",
+                            "header": "Name",
+                            cellFactory: (value, rowIdx, colIdx, rowValue) => {
+                                return <EditableCell key={colIdx} value={value} onChange={
+                                    (newName) => {
+                                        let newVenue = {... rowValue};
+                                        newVenue.name = newName;
+                                        executeAction("UpdateVenues", {updated_venues: [newVenue], tournament_id: tournamentId})
+                                    }
+                                } />
+                            }
+                        },
+                    ]
+                }
+            />
+        </div>
+        <div className="flex-none w-full h-12 bg-gray-200">
+            <button onClick={() => {
+                executeAction("UpdateVenues", {tournament_id: tournamentId, added_venues: [{uuid: "00000000-0000-0000-0000-000000000000", name: "New Venue", ordering_index: venues.venues.length}]})
+
+            }} className="h-full">Add Venue</button>
+        </div>
+    </div>
+}
